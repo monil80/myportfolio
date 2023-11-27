78d4baff3ef12d25e03eb51011c240c2ea583672
monil-ia <monil.nariya@infiniteai.in>
2023-11-27 15:27:27 +0530

    about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -43,8 +43,8 @@ const About = () => {
           </p>
           <p>
             If I need to define myself in one sentence that would be a family
-            person, father of a beautiful daughter, a sports fanatic,
-            photography enthusiast, and tech-obsessed!!!
+            person, father of a beautiful son, a sports fanatic,
+            and tech-obsessed!!!
           </p>
         </div>
 
